b09f92c5703105f7372607f48fdf76695de4b22f
XuRuntian <xuruntian03@163.com>
2026-02-07 14:26:41 +0800

    完善结构，优化文件名称

---
+++ source/robot_lab/robot_lab/tasks/manager_based/manipulation/wiping/config/booster_t1/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2024-2026 Ziqi Fan
+# SPDX-License-Identifier: Apache-2.0
+
+import gymnasium as gym, wipe_env_cfg
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="RobotLab-Isaac-Wiping-Booster-T1-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.flat_env_cfg:BoosterT1WipingEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:BoosterT1WipingPPORunnerCfg",
+    },
+)
